159d9e2e09071e77443cbb08f92388a9e5fefe97
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2025-02-10 19:42:15 -0800

    si

--- api/contact.js
+++ api/contact.js
@@ -7,6 +7,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // Or SUPABASE_ANON_KEY if you configured your RLS accordingly
 
+console.log('SUPABASE_URL:', supabaseUrl);
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
