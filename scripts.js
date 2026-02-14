a143b50ea5e6217f308e3e1c65613db582ee2da7
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2026-02-14 10:15:35 -0800

    worked on home page styles

--- scripts.js
+++ scripts.js
@@ -97,17 +97,3 @@ function createFloatingElements() {
   }
 }
 createFloatingElements();
-
-// Project Card Hover Effect
-document.querySelectorAll('.project-card').forEach(card => {
-  card.addEventListener('mousemove', (e) => {
-    const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    card.style.transform = `perspective(1000px) rotateX(${(y - rect.height / 2) / 10}deg) rotateY(${-(x - rect.width / 2) / 10}deg)`;
-  });
-
-  card.addEventListener('mouseleave', () => {
-    card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
-  });
-});
